d3e08da80991f13ffb9aff802812f330eefeba37
Timothy Sturges <tos3454@rit.edu>
2025-08-26 19:26:16 -0400

    View song is now view music, back button added for view music page

--- soundscape-ui/src/app/songs/songs.component.ts
+++ soundscape-ui/src/app/songs/songs.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SpotifyService } from '../spotify.service';
 import { UserService } from '../user.service';
 import { MessageService } from '../message.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-songs',
@@ -11,8 +12,12 @@ import { MessageService } from '../message.service';
   styleUrl: './songs.component.css'
 })
 export class SongsComponent{
-  
-  constructor(private spotifyService: SpotifyService, private userService: UserService, private messageService: MessageService){};
+
+  constructor(private router: Router,private spotifyService: SpotifyService, private userService: UserService, private messageService: MessageService){};
+
+  back(){
+    this.router.navigateByUrl('/account');
+  }
 
   getAlbums(){
     this.userService.getUser(localStorage.getItem("username") as string).subscribe((user) => {
